e423fed2a7f234e1f6c6d022b0d761a86b91ba59
Rafiere <rafapgbr@gmail.com>
2023-04-09 09:51:33 -0300

    feat: add lesson 02 code

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 
-	fmt.Println("Hello, world!")
+	var nome string = "Rafa"
+	var idade int = 21
+
+	/* Podemos declarar e atribuir valor com o operador ":=". */
+	versao := 1.0
+
+	fmt.Println("Hello world,", nome, "- Versão: ", versao, "- Idade: ", idade)
+	fmt.Println("O tipo da variável \"versao\" é: ", reflect.TypeOf(versao))
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("3 - Sair do Programa")
+
+	var comando int
+
+	/* Se a "comando" é do tipo "int", o "Scan" já identifica que queremos receber um "int". */
+	_, err := fmt.Scan(&comando)
+	fmt.Println(err)
+
+	fmt.Println("O comando escolhido foi:", comando)
+	fmt.Println("O endereço de memória da minha variável é:", &comando)
 }
